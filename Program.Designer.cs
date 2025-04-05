8304499ed838d0bfbfd4411efa4bcf2b7b439cc7
Abxxx25 <abigaileslao76@gmail.com>
2025-04-05 03:25:59 -0700

    Add files via upload

---
+++ Program.Designer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StudentRecordsApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StudentForm.Student_Page()); // Assuming your main form is named StudentPage
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,6 @@
+﻿namespace StudentForm
+{
+    public class Student
+    {
+    }
+}
